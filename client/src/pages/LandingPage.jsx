489abe25094f17749a1ab18db2302ff8bbe94463
CodeBruhhh <christianpescadero1@gmail.com>
2025-07-02 13:37:12 +0800

    Removed extra style from landing

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/landing-page-styles.css";
-// import "../styles/loginStyles.css";
 
 const LandingPage = () => {
 
